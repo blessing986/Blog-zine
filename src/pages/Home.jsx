495eda311c5055963ef2aa00ed02bcef9688e25d
Blessing U <blessing986@users.noreply.github.com>
2024-01-19 19:14:19 +0000

    chore: worked on styling the images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,46 @@
+import { useState } from "react";
 import img1 from "../assets/food1.jpg";
 import img2 from "../assets/food2.jpg";
 import img3 from "../assets/food3.jpg";
 import img4 from "../assets/food4.jpg";
-import styles from "./Home.module.css";
 
 export default function Home() {
+  const [hover, setHover] = useState(false);
+
+  function handleMouseOver() {
+    setHover((hover) => !hover);
+  }
+
+  function handleMouseOut() {
+    setHover((hover) => !hover);
+  }
+
+  let toggleClassCheck = hover ? "underline" : "";
+
   return (
     <>
       <div className="grid md:grid-cols-2 gap-10 mx-4 md:mx-16 md:my-10">
-        <div className="relative">
-          <div className="absolute top-40 md:top-1/2 ml-6 md:ml-10">
-            
+        <div
+          className="relative cursor-pointer"
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        >
+          <div className="w-full h-[400px] md:h-[808px] overflow-hidden rounded-3xl">
+            <img
+              src={img1}
+              alt="img1"
+              className="object-cover hover:scale-110 transition duration-500"
+            />
+          </div>
+
+          <div className="absolute top-40 md:top-1/2 ml-6 md:ml-10 text-white">
             <p className="flex items-center bg-red-600 w-28 py-0.5 md:py-2 rounded-lg font-medium md:font-bold text-white">
               <p className="w-4 h-4 bg-white rounded-full mx-2"></p>Lifestyle
             </p>
 
-            <h1 className="text-white text-2xl md:text-6xl font-bold my-4">
+            <h1
+              className={`text-white text-2xl md:text-6xl font-bold my-4 ${toggleClassCheck}`}
+            >
               Ten tell-tale signs you need to get a new startup.
             </h1>
 
@@ -25,91 +50,122 @@ export default function Home() {
             </p>
 
             <div className="hidden md:flex items-center text-white text-2xl font-medium">
-              <p className="">by Zero</p>
+              <p>
+                by <span className="hover:underline">Zero</span>{" "}
+              </p>
               <p className="w-1.5 h-1.5 bg-white rounded-full mx-6"></p>
               <p>Jan 16,2024</p>
             </div>
           </div>
-
-          <img src={img1} alt="img1" className={styles.img1} />
         </div>
 
         <div>
-          <div className="relative">
+          <div className="relative cursor-pointer"
+           onMouseOver={handleMouseOver}
+           onMouseOut={handleMouseOut}>
+            <div className="w-full h-96 overflow-hidden rounded-3xl mb-10">
+              <img
+                src={img2}
+                alt="img1"
+                className="object-cover hover:scale-110 transition duration-500"
+              />
+            </div>
+
             <div className="absolute top-56 md:top-40 ml-6 md:ml-10">
               <p className="flex items-center bg-yellow-300 w-32 py-0.5 md:py-2 rounded-lg font-medium md:font-bold text-white">
                 <p className="w-4 h-4 bg-white rounded-full mx-2"></p>Technology
               </p>
 
-              <h1 className="text-white text-2xl md:text-4xl font-bold my-4">
+              <h1 className={`text-white text-2xl md:text-4xl font-bold my-4 ${toggleClassCheck}`}>
                 Best Pinterest boards for learning about business
               </h1>
 
               <div className="hidden md:flex items-center text-white text-2xl font-medium">
-                <p className="">by Zero</p>
+                <p>
+                  by <span className="hover:underline">Zero</span>{" "}
+                </p>
                 <p className="w-1.5 h-1.5 bg-white rounded-full mx-6"></p>
                 <p>Jan 16,2024</p>
               </div>
             </div>
-
-            <img
-              src={img2}
-              alt="img1"
-              className="w-full h-96 object-cover mb-10 rounded-3xl"
-            />
           </div>
 
           <div className="grid md:grid-cols-2 gap-10">
-            <div className="relative">
+            <div className="relative cursor-pointer"
+             onMouseOver={handleMouseOver}
+             onMouseOut={handleMouseOut}>
+              <div className="w-full h-96 overflow-hidden rounded-3xl">
+                <img
+                  src={img3}
+                  alt="img1"
+                  className="object-cover hover:scale-110 transition duration-500"
+                />
+              </div>
               <div className="absolute top-56 md:top-40 ml-6 md:ml-10">
                 <p className="flex items-center bg-green-500 w-28 py-0.5 md:py-2 rounded-lg font-medium md:font-bold text-white">
                   <p className="w-4 h-4 bg-white rounded-full mx-2"></p>
                   Business
                 </p>
 
-                <h1 className="text-white text-2xl md:text-4xl font-bold my-4">
+                <h1 className={`text-white text-2xl md:text-4xl font-bold my-4 ${toggleClassCheck}`}>
                   Five intermediate guide to business
                 </h1>
 
                 <div className="hidden md:flex items-center text-white text-2xl font-medium">
-                  <p className="">by Zero</p>
+                  <p>
+                    by <span className="hover:underline">Zero</span>{" "}
+                  </p>
                   <p className="w-1.5 h-1.5 bg-white rounded-full mx-6"></p>
                   <p>Jan 16,2024</p>
                 </div>
               </div>
-
-              <img
-                src={img3}
-                alt="img1"
-                className="w-full h-96 object-cover rounded-3xl"
-              />
             </div>
-            <div className="relative">
+
+            <div className="relative cursor-pointer"
+             onMouseOver={handleMouseOver}
+             onMouseOut={handleMouseOut}>
+              <div className="w-full h-96 overflow-hidden rounded-3xl">
+                <img
+                  src={img4}
+                  alt="img1"
+                  className="object-cover hover:scale-110 transition duration-500"
+                />
+              </div>
               <div className="absolute top-56 md:top-40 ml-6 md:ml-10">
                 <p className="flex items-center bg-blue-500 w-28 py-0.5 md:py-2 rounded-lg font-medium md:font-bold text-white">
                   <p className="w-4 h-4 bg-white rounded-full mx-2"></p>
                   Sports
                 </p>
 
-                <h1 className="text-white text-2xl md:text-4xl font-bold my-4">
-                  15 reasons to choose <br/> startup
+                <h1 className={`text-white text-2xl md:text-4xl font-bold my-4 ${toggleClassCheck}`}>
+                  15 reasons to choose <br /> startup
                 </h1>
 
                 <div className="hidden md:flex items-center text-white text-2xl font-medium">
-                  <p className="">by Zero</p>
+                  <p>
+                    by <span className="hover:underline">Zero</span>{" "}
+                  </p>
                   <p className="w-1.5 h-1.5 bg-white rounded-full mx-6"></p>
                   <p>Jan 16,2024</p>
                 </div>
               </div>
-              <img
-                src={img4}
-                alt="img1"
-                className="w-full h-96 object-cover rounded-3xl"
-              />
             </div>
           </div>
         </div>
       </div>
+
+      {/* <div className="relative">
+        <div class="max-w-[300px] max-h-[300px] overflow-hidden rounded-3xl">
+          <img
+            src={img2}
+            className="hover:scale-110 transition duration-500 cursor-pointer object-cover"
+            alt="jj"
+          />
+        </div>
+        <div>
+          <h1 className="absolute top-2">i love you</h1>
+        </div>
+      </div> */}
     </>
   );
 }
