4cfc604e38bec1e22b3d45033a09cbac4ac82792
Blessing U <blessing986@users.noreply.github.com>
2024-01-21 16:23:46 +0000

    chore: added a button

--- src/pages/TopHighlights.jsx
+++ src/pages/TopHighlights.jsx
@@ -24,9 +24,29 @@ export default function TopHighlights() {
           Latest breaking news, pictures, videos, and special reports
         </p>
       </div>
-      <div class="grid md:grid-cols-2 md:w-3/4">{TopHighlightsList}</div>
+      <div className="md:w-3/4">
+        <div class="grid md:grid-cols-2">{TopHighlightsList}</div>
 
-      <button>Load more post</button>
+        <div className="flex justify-center items-center mb-10">
+          <button className="flex items-center justify-center bg-sky-100 text-sky-700 px-6 py-4 text-2xl font-bold rounded-lg">
+            Load more post{" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6 ml-4"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>{" "}
+          </button>
+        </div>
+      </div>
     </>
   );
 }
