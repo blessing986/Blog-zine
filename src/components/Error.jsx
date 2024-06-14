faf0c7729979c93abcf59eaa9acaf54e2c591f12
Blessing U <blessing986@users.noreply.github.com>
2024-06-14 11:24:04 +0100

    chore: added text to the error component.

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,10 +1,12 @@
-export default function Error({ title, message }) {
+export default function Error() {
   return (
     <>
       <div className="flex flex-col justify-center items-center text-sky-700">
         <div className="bg-sky-100 w-96 text-center py-10 mt-44">
-          <h2 className="text-xl font-bold">{title}</h2>
-          <p className="text-base font-medium">{message}</p>
+          <h2 className="text-xl font-bold">An error occured!</h2>
+          <p className="text-base font-medium">
+            Could not fetch post, please try again later.
+          </p>
         </div>
       </div>
     </>
